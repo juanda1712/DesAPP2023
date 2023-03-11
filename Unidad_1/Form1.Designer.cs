b75bcf4a4afdeeef4b3336aa480042b3280cbcaf
JUAN DAVID........ LOPEZ <juanda-1712@hotmail.com>
2023-03-11 14:15:58 -0500

    Agregar archivos de proyecto.

---
+++ Unidad_1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Unidad_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtCC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 86);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Aceptar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtCC
+            // 
+            this.txtCC.Location = new System.Drawing.Point(322, 124);
+            this.txtCC.Name = "txtCC";
+            this.txtCC.Size = new System.Drawing.Size(225, 22);
+            this.txtCC.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CC";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(913, 521);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCC);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtCC;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Unidad_1/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad_1
+{
+    public partial class Form1 : Form
+    {
+        string Direccion;
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Declaraciones de variable
+            int numero = 10;
+            int codigo = 3652526; 
+            int num1, num2, num3;
+            long lg;
+            double dec;
+            decimal dec2;
+
+            string nombre = "Juan David ";
+            string apellido;
+            string edad = "30";
+            bool bl1 = true;
+            DateTime fecha;
+
+
+            /// VAriable implicita 
+            /// 
+            var objeto = "carro" ;
+            objeto = "moto";
+            ////
+            ///
+
+            Direccion = "cr 12 n 10-35";
+            num1 = 2;
+
+
+
+            ///conversiones 
+            ///
+
+            lg = num1 ;
+            num1 = int.Parse(edad);
+            edad = num1.ToString();
+
+            num1 = Convert.ToInt32(edad);
+            dec = Convert.ToDouble(edad);
+            dec2 = Convert.ToDecimal(edad);
+            //bl1 = Convert.ToBoolean(edad);
+            edad = bl1.ToString();
+
+
+
+            // funciones del string 
+
+            //cant de caractres
+            var cant_caracteres = nombre.Length;
+
+            // sub string  p1 = inicia (0) , p2 = donde finalizo (3) = 
+            var primeros_caracteres = nombre.Substring(0, 3);
+
+            // quita los caracteres en blanco 
+            var nombre_sin_espacios = nombre.Trim();
+
+            // concatenar  
+
+            var nombre_dir = string.Concat(nombre, " ", Direccion);
+
+            var nombre_dir_2 = string.Format(" el nombre es: {0} y la direccion es {1} ", nombre, Direccion);
+
+            /// Operadores
+            /// 
+
+            num1 = num1 + 1;
+            num1++;
+            --num1;
+            num1 += 20;
+
+
+
+
+        }
+
+        /// <summary>
+        /// /metodo sin retorno  --> VOID
+        /// </summary>
+        private void mostrarMsj(string texto)
+        {
+            MessageBox.Show(string.Concat("Hola mundo   ", texto));
+
+        }
+
+
+        /// <summary>
+        /// paramatros opcionales
+        /// </summary>
+        /// <param name="cc"></param>
+        /// <param name="tel"></param>
+        /// <param name="PT"></param>
+        /// <returns></returns>
+        private string consultar_nombre(int cc, int tel = 0, string PT = "" )
+        {
+            string nombre = "Juan";                    
+            
+            /// condicionales 
+            /// Operadores -> ==, dif-> != , AND -> &&  OR -> ||  
+            /// 
+            if(cc == 1093 && tel == 123 )
+            {
+                nombre = "Carlos";
+            }
+            else if(cc == 1093 || tel == 0)
+            {
+                nombre = "Lina";
+            }
+          
+            else 
+            {
+                nombre = "David";
+            
+            }
+
+            return nombre;
+        }
+
+
+        /// <summary>
+        /// swiche
+        /// </summary>
+        /// <param name="num_dia"></param>
+        /// <returns></returns>
+        private string consultar_dia_texto(int num_dia)
+        {
+            string dia= "";
+            switch(num_dia)
+            {
+                case 1:
+                    dia = "Lunes";
+                    break;
+                case 2:
+                    dia = "Martes";
+                    break;
+                case 3:
+                    dia = "Miercoles";
+                    break;
+                case 4:
+                    dia = "Jueves";
+                    break;
+
+                    default:
+                    dia = "No aplica";
+                    break;
+            }
+            
+            return dia;                
+        }
+
+        private int suma(int num1 , int  num2)
+        {
+            return num1+ num2;
+        }
+
+
+        /// <summary>
+        /// Cliclos -->  While , Do While , For
+        /// </summary>
+        private void ciclos()
+        {
+
+            int a = 20;
+            while (a >= 10)
+            {
+                a --;
+            }
+
+            do
+            {
+                a--;
+                MessageBox.Show("");
+            }
+            while (a >=10);
+
+
+
+            // For
+
+            for(int b = 0; b<= 30; b++)
+            {
+                MessageBox.Show("");
+                /// Código
+
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Vectores y matrices
+        /// </summary>
+        private void Arrays()
+        {
+            int[] vectResult;
+
+            vectResult = new int[10]; // cant posiciones 
+
+            vectResult = new int[5] { 1,2,3,4,5}; // set values  for default
+
+            Array.Resize<int>(ref vectResult, 10);  // aumenta el tamaño del array
+
+            vectResult[0] = 1;
+            vectResult[1] = 2;
+            vectResult[2] = 3;
+
+            int[,] mtResulta;
+            mtResulta = new int[3, 3];
+            mtResulta = new int[2, 2] { { 1, 3 }, { 4, 5 } };
+
+        }
+
+
+
+        private void Listas()
+        {
+            List<string> meses = new List<string>() { "diciembre", "noviembre"};  /// default
+
+            meses.Add("enero");  // agg items 
+            meses.Add("febrero");
+            meses.Add("marzo");
+            meses.Add("abril");
+
+
+            string mes = meses[0];
+
+            meses.Remove("enero");  // remove por detalle
+            meses.RemoveAt(1);  // remove por indece
+
+
+            for (int i = 0; i< meses.Count; i++)
+            {
+                MessageBox.Show(meses[i]);
+            }
+
+
+
+        }
+
+
+        /// <summary>
+        /// overloding  / Sobrecarga 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        private double suma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cc = 0;            
+            
+
+
+            if (txtCC.Text != "")
+            {
+                cc = Convert.ToInt32(txtCC.Text);
+
+                //var con_nombre = consultar_nombre(cc, 123); // llamado de una funcion 
+
+                //// llamado de un metodo
+                //mostrarMsj(con_nombre);
+
+                string dia = consultar_dia_texto(cc);
+                mostrarMsj(dia);
+            }
+           
+          
+
+        }
+    }
+}
